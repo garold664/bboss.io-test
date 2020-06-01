cda93941c91fbfee884bb78ac3e3895ed1bf3df8
garold664 <garold664@gmail.com>
2020-06-01 10:11:19 +0300

    улучшение мобильного меню

--- build/js/script.js
+++ build/js/script.js
@@ -4,6 +4,20 @@ var body = document.body;
 
 navBtn.addEventListener('click', function (e) {
   var curTarget = e.currentTarget;
-  curTarget.classList.toggle('header__nav-btn--active');
-  body.classList.toggle('menu-opened');
+
+  if (body.classList.contains('menu-closed')) {
+
+    body.style.top = (parseInt(window.scrollY, 10) * -1) + 'px';
+    curTarget.classList.add('header__nav-btn--active');
+    body.classList.remove('menu-closed');
+
+    // body.style.position = 'fixed';
+  } else {
+    var scrollY = body.style.top;
+    curTarget.classList.remove('header__nav-btn--active');
+    body.classList.add('menu-closed');
+
+    body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+  }
 });
