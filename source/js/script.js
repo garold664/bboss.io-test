911af075678da7c3c2cd0881e1c90c60d91c6e69
garold664 <garold664@gmail.com>
2020-06-01 11:48:45 +0300

    правит анимацию бургер-кнопки

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,13 @@
 'use strict';
 var navBtn = document.querySelector('.header__nav-btn');
+var nav = document.querySelector('.header__nav');
 var body = document.body;
+
+nav.style.transition = 'none';
+body.classList.remove('no-js');
+setTimeout(function () {
+  nav.style.transition = '';
+}, 300);
 
 navBtn.addEventListener('click', function (e) {
   var curTarget = e.currentTarget;
